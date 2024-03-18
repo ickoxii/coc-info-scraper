8f625249fccad0ee046fa13f8f9073293f8740ab
Icko <80863308+ickoxii@users.noreply.github.com>
2024-03-18 17:22:11 -0500

    gpl-v3 license disclaimer

--- src/main/java/io/github/ickoxii/interfaces/implementations/JWTAPIImpl.java
+++ src/main/java/io/github/ickoxii/interfaces/implementations/JWTAPIImpl.java
@@ -1,3 +1,22 @@
+ /**
+    Author: Icko Iben
+    Date Created: 3/16/2024
+    Version: 1.0.0
+
+    This file is part of CocInfoScraper.
+
+    CocInfoScraper is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later version.
+
+    CocInfoScraper is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with
+    CocInfoScraper. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.ickoxii.interfaces.implementations;
 
 import io.github.ickoxii.interfaces.api.JWTAPIController;
